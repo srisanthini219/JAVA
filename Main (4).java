c7d653a1b7fcb42316dd3d6cf3027dc82a8fd587
srisanthini219 <23becse219@kahedu.edu.in>
2025-12-13 10:25:38 +0530

    Add files via upload

---
+++ Main (4).java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+     public static void main(String[]args){
+         Scanner sc = new Scanner(System.in);
+         int a=10;
+         int b=20;
+         float c=a/(float)b;
+        System.out.println(c);
+     }
+}
